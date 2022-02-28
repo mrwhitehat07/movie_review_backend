eba38432ac27a854e994f38f6f08cb1cd29e3309
mrwhitehat07 <bishalmagr70@gmail.com>
2022-02-28 11:10:29 +0545

    serializer updated

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,30 +1,44 @@
 from statistics import mode
 from django.forms import fields
 from rest_framework import serializers
+
+from user.serializers import ProfileSeriL
 from .models import Review
-from user.models import UserProfile
+from user.models import Profile, UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = UserProfile
-        fields = ['username']
+        fields = '__all__'
+    
+    # def to_representation(self, instance):
+    #     return {
+    #         'id': instance.id,
+    #         'profile': instance.profile,
+    #     }
 
+class UProfileSerial(serializers.ModelSerializer):
+    user = UserProfileSerializer()
+    class Meta:
+        model = Profile
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # user = UserProfileSerializer()
+    # user = UProfileSerial()
     class Meta:
         model = Review
-        fields = ['rating', 'review', 'timestamp']
+        fields = ['id','rating', 'review', 'timestamp']
         # fields = '__all__'
 
     # def to_representation(self, value):
     #     return {
     #             'id': value.id,
-    #             'fname': value.user.profile,
-    #             # 'lname': value.celebs.lname,
-    #             # 'image': value.celebs.image.url,
-    #             # 'role' : value.role.name,
-    #         }
+    #             'fname': value.user,
+                # 'lname': value.celebs.lname,
+                # 'image': value.celebs.image.url,
+                # 'role' : value.role.name,
+            # }
 
 class UpdateReviewSerializer(serializers.ModelSerializer):
     class Meta:
